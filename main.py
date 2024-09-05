6689c1961fe73fe1caed8bed32cc0d497eda45d6
Norbert <54719382+NorbertCode@users.noreply.github.com>
2024-09-05 15:45:27 +0200

    Add main.py

---
+++ main.py
@@ -0,0 +1,38 @@
+import sys
+from PIL import Image
+
+def grayscale(rgb):
+    # If the pixel is transparent treat it like it's white
+    if len(rgb) > 3 and rgb[3] == 0:
+        return 255
+    else: # Otherwise average the rgb values
+        return sum(rgb[:3]) // 3
+
+thresholdsPerChar = {
+    255: " ",
+    204: ".",
+    153: "-",
+    102: "/",
+    51: "%",
+    1: "@",
+    0: "#"
+}
+
+# Read the image
+imageName = ""
+if len(sys.argv) > 1: # So if there's an argument given
+    imageName = sys.argv[1]
+else:
+    imageName = input("Please enter image path: ")
+
+image = Image.open(imageName)
+imageSize = image.size
+
+# Iterate through all pixels
+for y in range(imageSize[1]):
+    for x in range(imageSize[0]):
+        for key, value in thresholdsPerChar.items():
+            if grayscale(image.getpixel([x, y])) >= key: # Grayscale them and print the corresponding character
+                print(value, end='')
+                break
+    print("\n", end='')
