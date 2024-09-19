cb5d3a10af03ff47bc96e810766396acbc77b0c1
Norbert <54719382+NorbertCode@users.noreply.github.com>
2024-09-19 02:07:15 +0200

    Basic functionality

--- main.py
+++ main.py
@@ -1,17 +1,24 @@
-from flask import Flask, render_template, request
+from flask import Flask, redirect, render_template, request
 import generator
 
 app = Flask(__name__)
 
-@app.route("/", methods=["GET", "POST"])
-def hello_world():
-    if request.method == "POST":
-        file = request.files.get("image", None)
-        if file:
-            print(generator.generate(file))
-        return ""
+@app.route("/")
+def index():
+    return render_template("index.html", title="Main Page")
+
+@app.route("/result", methods=["POST"])
+def result():
+    file = request.files.get("image", None)
+    if file:
+        ascii = generator.generate(file)
+        return render_template("result.html", title="Result", ascii=ascii)
     else:
-        return render_template("index.html", title="Main Page")
+        return redirect("/error")
+
+@app.route("/error")
+def error():
+    return render_template("error.html", title="Error")
 
 if __name__ == "__main__":
     app.run(debug=True)
