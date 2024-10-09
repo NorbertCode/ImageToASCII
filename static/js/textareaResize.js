5e3e15895fd0b5febd487020c7b2a935dc4aba00
Norbert <54719382+NorbertCode@users.noreply.github.com>
2024-10-09 20:20:16 +0200

    Fixed result textarea height getting set incorrectly

--- static/js/textareaResize.js
+++ static/js/textareaResize.js
@@ -1,10 +1,15 @@
-const textarea = document.querySelector("textarea")
-let textRows = textarea.value.split("\n");
-
-let textWidth = textRows[0].length;
-let textHeight = textRows.length - 1;
-let newSize = Math.max(-1/6 * textWidth + 48, 4);
-
-textarea.style.fontSize = `${newSize}px`;
-textarea.style.lineHeight = `${newSize}px`;
-textarea.rows = textHeight + 1;
+window.addEventListener("load", function () {
+    const textarea = document.querySelector("textarea")
+    let textRows = textarea.value.split("\n");
+    
+    let textWidth = textRows[0].length;
+    let textHeight = textRows.length - 1;
+    let newSize = Math.max(-1/6 * textWidth + 48, 4);
+    
+    console.log(textWidth, textHeight, newSize);
+    
+    textarea.style.fontSize = `${newSize}px`;
+    textarea.style.lineHeight = `${newSize}px`;
+    textarea.rows = textHeight + 1;
+    textarea.style.height = `${textarea.rows * newSize}px`;
+})
