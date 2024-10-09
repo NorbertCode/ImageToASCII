f5689d9685c8f7707f3410bdc366163d03bd4940
Norbert <54719382+NorbertCode@users.noreply.github.com>
2024-10-09 19:22:52 +0200

    Cleanup and comments

--- static/js/imagePreview.js
+++ static/js/imagePreview.js
@@ -1,29 +1,38 @@
 function PreviewImage() {
+    const imageInput = document.querySelector("#image");
+
     let fileReader = new FileReader();
-    fileReader.readAsDataURL(document.querySelector("#image").files[0]);
+    fileReader.readAsDataURL(imageInput.files[0]);
 
     fileReader.onload = function (fileReaderEvent) {
         let preview = document.querySelector("#preview");
         if (preview == null) {
+            const previewHolder = document.querySelector("#previewHolder");
+
+            // Disable selection feedback
             const selectionFeedback = document.querySelector("#selectionFeedback");
             selectionFeedback.style.display = "none";
 
+            // Create preview image
             preview = document.createElement("img");
             preview.setAttribute("id", "preview");
-            document.querySelector("#previewHolder").appendChild(preview);
+            previewHolder.appendChild(preview);
 
+            // Create and initialize cancel button
             const cancelButton = document.createElement("input");
             cancelButton.setAttribute("type", "button");
             cancelButton.setAttribute("class", "cancelButton");
             cancelButton.value = "REMOVE IMAGE";
             cancelButton.onclick = function () {
+                // Show selection feedback again
                 selectionFeedback.style.display = "block";
 
-                document.querySelector("#image").value = "";
+                // Remove all unnecessary elements
+                imageInput.value = "";
                 preview.remove();
                 cancelButton.remove();
             }
-            document.querySelector("#previewHolder").appendChild(cancelButton);
+            previewHolder.appendChild(cancelButton);
         }
 
         preview.src = fileReaderEvent.target.result;
